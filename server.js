c81ec5d2ae079d7720469ad500208e43e9c79a8e
itsmeajay1699 <112202679+itsmeajay1699@users.noreply.github.com>
2023-05-17 19:14:45 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app, { json } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import  routerAuth  from "./routes/userRoutes.js";
+import routerAuth from "./routes/userRoutes.js";
 const server = app();
 
 // connect to database
@@ -10,7 +10,11 @@ connectDB();
 
 server.use(morgan("dev"));
 server.use(json());
-server.use(cors());
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // routes
 server.use("/api/v1/auth", routerAuth);
